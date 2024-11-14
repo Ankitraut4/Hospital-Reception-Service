fb97e3d5006c1a704795e94ec1c8016964823013
Ankit Raut <ankitraut9421@gmail.com>
2024-11-14 01:00:16 -0500

    add files

---
+++ server/src/main/resources/data.sql
@@ -0,0 +1,12 @@
+
+
+INSERT INTO doctor (`id`,`age`,`field`,`gender`,`name`,`patient_count`) VALUES (1,35,'Gynecologists','female','Dr.Anjali Raut',1);
+INSERT INTO doctor (`id`,`age`,`field`,`gender`,`name`,`patient_count`) VALUES (2,51,'Orthopaedic','male','Dr. Yuoraj Raut',1);
+INSERT INTO doctor (`id`,`age`,`field`,`gender`,`name`,`patient_count`) VALUES (3,32,'Edocrinologists','female','Dr.Mukta Raut',1);
+
+INSERT INTO patient (`id`,`age`,`date_of_visit`,`doctor_name`,`gender`,`name`,`prescription`) VALUES (5,30,'2022-05-23','Dr.Yuoraj Raut','male','Ashish Raut','https://www.researchgate.net/publication/345830022/figure/fig4/AS:957640029003789@1605330583881/Sample-prescription-used-as-input-to-the-GUI-developed-in-the-present-work.png');
+INSERT INTO patient (`id`,`age`,`date_of_visit`,`doctor_name`,`gender`,`name`,`prescription`) VALUES (6,30,'2022-05-23','Dr.mukta Raut ','male','Pavan Raut','https://images.thequint.com/thequint%2F2020-06%2Fef4e2f21-d473-47bf-ba6d-552b7b9d7cee%2FAdd_a_subheading__1_.jpg');
+INSERT INTO patient (`id`,`age`,`date_of_visit`,`doctor_name`,`gender`,`name`,`prescription`) VALUES (7,28,'2022-05-23','Dr.Anjali Raut','female','Pradhnya Raut','https://cdn3.mycity4kids.com/images/article-images/web/headersV2/img-20200128-5e305cd80566f.png');
+
+
+
